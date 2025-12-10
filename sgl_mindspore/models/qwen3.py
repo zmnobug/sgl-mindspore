1a624b8a0a5664b6b0eae18c0520cafea25230e2
zm <zhaomin88@huawei.com>
2025-12-10 11:53:02 +0800

    modify set_model_inputs in Qwen3ForCausalLM

--- sgl_mindspore/models/qwen3.py
+++ sgl_mindspore/models/qwen3.py
@@ -451,7 +451,8 @@ class Qwen3ForCausalLM(MindSporeModelBase):
 
         head_size = self.config.head_dim
         # use pa, if use ifa, the shape should (None, None, head_size)
-        kv_cache_shape = (None, None, None, head_size)
+        num_kv_heads = self.config.num_key_value_heads // self.tp_size
+        kv_cache_shape = (None, None, num_kv_heads * head_size)
 
         kv_cache_dtype = self.config.param_dtype
 
